3956bfd81e206558faf9d1cc6068260b4b9c330a
Rodrigo Alejandro Gomez Cerda <rodrigo@MacBook-Air-de-Rodrigo.local>
2020-03-19 01:27:00 -0300

    UPDATE: change code lifecycle to hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from './components';
 import Router from './Router';
 
 const App = () => {
-  useEffect(() => {
-    const localFavs = localStorage.getItem('favorite');
-    if (!localFavs) {
-      const initialFavorite = [
-        {
-          name: 'Piratas',
-          favorite: true
-        },
-        {
-          name: 'Tiburones',
-          favorite: true
-        }
-      ];
-      localStorage.setItem('favorite', JSON.stringify(initialFavorite));
-    }
-  }, []);
-
   return (
     <div className='App'>
       <Header />
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,66 +1,97 @@
 import React, { useState, useEffect } from 'react';
 
-import { StarBorderOutlined, StarRate, Favorite } from '@material-ui/icons';
+import { StarBorderOutlined, StarRate } from '@material-ui/icons';
 
 import './styles.scss';
 
 const Team = props => {
   const { logo, name } = props;
-  const [fav, setFav] = useState(false);
-  const [dataLocalStorage, setdataLocalStorage] = useState([{}]);
-
+  const [favorite, setFavorite] = useState({
+    state: '',
+    dataLocalStorage: []
+  });
   useEffect(() => {
     const localFavorite = JSON.parse(localStorage.getItem('favorite'));
-    setdataLocalStorage(localFavorite);
     if (localFavorite) {
-      const favorite = localFavorite.find(team => team.name === name);
-      if (favorite) {
-        if (favorite.favorite) {
-          setFav(true);
+      const favoriteValue = localFavorite.find(team => {
+        if (team.name === name) {
+          return team.favorite;
         } else {
-          setFav(false);
+          return false;
         }
+      });
+      if (favoriteValue) {
+        setFavorite({
+          state: true,
+          dataLocalStorage: favorite.dataLocalStorage
+        });
       } else {
-        setFav(false);
+        setFavorite({
+          state: false,
+          dataLocalStorage: favorite.dataLocalStorage
+        });
       }
     }
   }, [name]);
 
-  const handleClick = () => {
-    if (dataLocalStorage) {
-      const favorite = dataLocalStorage.find(team => team.name === name);
-      if (!favorite) {
-        const favoriteNewInArray = [
-          ...dataLocalStorage,
+  useEffect(() => {
+    let teamNewInArray = [];
+    const localFavorite = JSON.parse(localStorage.getItem('favorite'));
+    if (localFavorite && favorite.dataLocalStorage.length > 0) {
+      const foundTeam = favorite.dataLocalStorage.find(
+        team => team.name === name
+      );
+      if (!foundTeam) {
+        teamNewInArray = [
+          ...favorite.dataLocalStorage,
           {
             name: name,
             favorite: true
           }
         ];
-        localStorage.setItem('favorite', JSON.stringify(favoriteNewInArray));
+      } else {
+        teamNewInArray = favorite.dataLocalStorage.map(team => {
+          let favoriteValue = team.favorite;
+          if (team.name === name) {
+            favoriteValue = !team.favorite;
+          }
+          return {
+            ...team,
+            favorite: favoriteValue
+          };
+        });
       }
+      localStorage.setItem('favorite', JSON.stringify(teamNewInArray));
+    } else if (favorite.state !== '') {
+      teamNewInArray = [
+        {
+          name: name,
+          favorite: true
+        }
+      ];
+      localStorage.setItem('favorite', JSON.stringify(teamNewInArray));
     }
+  }, [favorite, name]);
 
-    const favoriteNew = dataLocalStorage.map(team => {
-      let favoriteValue = team.favorite;
-      if (team.name === name) {
-        favoriteValue = !team.favorite;
-      }
-      return {
-        ...team,
-        favorite: favoriteValue
-      };
-    });
-    setFav(!fav);
-    setdataLocalStorage(favoriteNew);
-    console.log(favoriteNew);
-    localStorage.setItem('favorite', JSON.stringify(favoriteNew));
+  const handleClick = () => {
+    const localFavorite = JSON.parse(localStorage.getItem('favorite'));
+    if (localFavorite) {
+      setFavorite({
+        state: !favorite.state,
+        dataLocalStorage: localFavorite
+      });
+    } else {
+      setFavorite({
+        state: !favorite.state,
+        dataLocalStorage: favorite.dataLocalStorage
+      });
+    }
   };
 
   return (
     <div>
       <img className='team-logo' src={logo} alt={name} />
-      {fav ? (
+      {favorite.state ? (
         <StarRate onClick={handleClick} />
       ) : (
         <StarBorderOutlined onClick={handleClick} />
