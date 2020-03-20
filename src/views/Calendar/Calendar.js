63821b9f846358405242a373ac36780a3c38fcca
Rodrigo Alejandro Gomez Cerda <rodrigo@MacBook-Air-de-Rodrigo.local>
2020-03-20 18:03:14 -0300

    ADD: create view calendar and league

---
+++ src/views/Calendar/Calendar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Game } from '../../components';
+
+import LogoFelines from '../../assets/logos/felinos.png';
+import LogoPirates from '../../assets/logos/piratas.png';
+import LogoShark from '../../assets/logos/tiburones.png';
+
+import './styles.scss';
+
+const Calendar = props => {
+  const { match } = props;
+  return (
+    <div className='calendars'>
+      Team: {match.params.team}
+      <Game
+        localeName='Felinos'
+        localeLogo={LogoFelines}
+        localeAnotation='2'
+        visitorName='Piratas'
+        visitorLogo={LogoPirates}
+        visitorAnotation='1'
+        stadium='Estadio Olímpico'
+        date='20-Abril-2018'
+        time='8:00 pm'
+      />
+      <Game
+        localeName='Piratas'
+        localeLogo={LogoPirates}
+        localeAnotation='2'
+        visitorName='Tiburones'
+        visitorLogo={LogoShark}
+        visitorAnotation='2'
+        stadium='Estadio Universitario'
+        date='24-Abril-2018'
+        time='7:00 pm'
+      />
+    </div>
+  );
+};
+
+export default Calendar;
---
+++ src/views/League/League.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+import { Team } from '../../components';
+
+import LogoFelines from './../../assets/logos/felinos.png';
+import LogoPirates from './../../assets/logos/piratas.png';
+import LogoSharks from './../../assets/logos/tiburones.png';
+
+const League = () => {
+  const [data, setData] = useState([]);
+  const [url] = useState('https://api-mi-liga.now.sh/api/jugadores');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  /* useEffect(() => {
+    const fetchApi = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchApi();
+
+    return () => setData([]);
+  }, [url]); */
+  return (
+    <div className='container'>
+      <div className='list-team'>
+        <Team name='Felinos' logo={LogoFelines} />
+        <Team name='Piratas' logo={LogoPirates} />
+        <Team name='Tiburones' logo={LogoSharks} />
+      </div>
+      <div className='list-player'>
+        {/* {isError && <div>Something went wrong ...</div>}
+        {isLoading ? (
+          <div>Loading ...</div>
+        ) : (
+          data.map((player, index) => {
+            return (
+              <Player
+                key={index}
+                name={player.nombre}
+                photo={require('../../assets/photo/' + player.foto)}
+              />
+            );
+          })
+        )} */}
+      </div>
+    </div>
+  );
+};
+
+export default League;
